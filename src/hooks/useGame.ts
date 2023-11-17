e6b6335b063478be7dee1e8a7bb16f32909d01d9
WAHJADAI <drafzaza123@icloud.com>
2023-11-17 19:02:16 +0700

    Implement infinite scroll

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -29,4 +29,5 @@ export const useGame = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
+    staleTime: 24 * 60 * 60 * 1000,
   });
