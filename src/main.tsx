aa263a33cc0696dc231c0586b798b8decabefb43
WAHJADAI <drafzaza123@icloud.com>
2023-11-21 19:49:10 +0700

    Setup routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,22 @@
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import App from "./App";
-import theme from "./theme";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import { QueryClient,QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import router from "./routes";
+import theme from "./theme";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript  initialColorMode={theme.config.initialColorMode}/>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools/>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
