91db55c83d653a1852e34ecab982dfe7d81b16ed
WAHJADAI <drafzaza123@icloud.com>
2023-11-22 19:56:50 +0700

    Build game attributes

--- src/entity/Game.ts
+++ src/entity/Game.ts
@@ -1,9 +1,13 @@
+import { Genres } from "./Genres";
 import { Platform } from "./Platform";
+import { publishers } from "./Publisher";
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres:Genres[];
+  publishers:publishers[];
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform; }[];
